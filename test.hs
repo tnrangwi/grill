f28459e6e9b004bca690b94b2fef57ab1b593c94
tnrangwi <trtoaster@googlemail.com>
2011-08-13 20:28:48 +0200

    More doc in main test script

--- test.hs
+++ test.hs
@@ -11,8 +11,7 @@ import qualified FormulaEngine.Parse as Parse
 
 -- Fixme: How to treat different input types accordingly?
 
--- | Test function to add values
-
+-- | Test all functions implemented currently. FIXME: There should be a unit test instead.
 main :: IO ()
 main = do
   print "Evaluating raw formula tree"
@@ -20,16 +19,16 @@ main = do
   print (Eval.eval rawtree)
   print "Done!"
 
-  print "Compiling and evaluating add tree with int"
+  print "Compiling and evaluating add tree with int - should give 6"
   let stringtree1 = Parse.compile "(add 1 (add 2 3))"
   print (Eval.eval stringtree1)
 
-  print "Compiling and evaluating add tree with float"
+  print "Compiling and evaluating add tree with float - should give 6.3"
   let stringtree2 = Parse.compile "(add 1.0 (add 2.1 3.2))"
   print (Eval.eval stringtree2)
 
-  print "Compiling and evaluating string function"
+  print "Compiling and evaluating string function containing escaped quotes"
   let stringtree3 = Parse.compile "(conc \"A\"\"\" \"BC\")"
   print (Eval.eval stringtree3)
 
-  print "Done"
+  print "All done successfully"
