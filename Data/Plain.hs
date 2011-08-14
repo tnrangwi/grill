da072535c1e26e1bf3714f80fe6b397a045b761b
tnrangwi <trtoaster@googlemail.com>
2011-08-14 20:45:15 +0200

    Get Plain documentation in a reasonable state for haddock

--- Data/Plain.hs
+++ Data/Plain.hs
@@ -1,25 +1,31 @@
--- | Author: Thorsten Rangwich
--- | See file LICENSE for details.
--- | This file defines the tree structure used for the formulas.
+-- | This file defines 'Plain', the base type used for the calculation trees.
+--
+--   Author: Thorsten Rangwich. See file LICENSE for details.
+
 module Data.Plain
 (
- GenericResult,
+ -- * Base Data Types supported in Tree
  Plain(..),
+ -- * Type Classes for Base Data Types
  Convert(..),
- PlainFunction
+ -- * Tree Function Interface
+ PlainFunction,
+ -- * Experimental
+ GenericResult -- export more of it?
 )
 
 where
 
--- | Generic object - this is to hold results that cannot be shown (like the result of an external library.
--- | I currently don't know how this should be implemented but I am sure we will need that even for
--- | internal use.
-
+-- | Generic object - this is to hold results that cannot be shown (like the result of an external library).
+-- I currently don\'t know how this should be implemented but I am sure we will need that even for
+-- internal use.
+-- FIXME: Use a special type class instead with methods for display in cell,
+-- possibly serialisation or whatever will be needed.
 data GenericResult = GResError String -- ^ Error returned, information in string
                    | GResXYZ Int -- ^ Example to test implementation
                      deriving Show
 
--- | Plain data type containing some plain value or a composite result that should not be evaluated further
+-- | Plain data type containing some plain value or a composite result that should not be evaluated further.
 data Plain = PlEmpty -- ^ Empty item
            | PlInt Int -- ^ Int value
            | PlFloat Float -- ^ Float value
@@ -28,16 +34,20 @@ data Plain = PlEmpty -- ^ Empty item
            | PlComposite GenericResult
              deriving Show
 
+-- | Function type for every tree function. Input takes a list of plains and returns one Plain value.
 type PlainFunction = ([Plain] -> Plain)
 
--- | Convert Plain into raw data type and vice versa
+-- | Class to convert Plain into raw data type and vice versa.
 class Convert a where
-    -- | Convert Plain data type into its raw data type
+    -- | Convert Plain data type into its raw data type. Has to raise error in default implementation.
     get :: Plain -- ^ Input Plain
         -> a -- ^ Raw data type for return
-    -- | Standard implementation - raise error
+
     get _ = error "Unexpected type - add instance declaration to unpack 'Plain' type"
-    toPlain :: a -> Plain
+
+    -- | Convert arbitrary data type into a packaged Plain. Default implementation returns PlError.
+    toPlain :: a -- ^ Input arbitrary data type.
+            -> Plain -- ^ Output - packaged in Plain.
     toPlain _ = PlError "No 'Plain' implementation for this data type"
 
 
