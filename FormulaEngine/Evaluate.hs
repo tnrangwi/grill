b4c37abf81fc99dcdd20522f0abd5ff13a1779c8
tnrangwi <trtoaster@googlemail.com>
2011-09-21 21:15:22 +0200

    Evaluate of sheet cell fully implemented. Now I can merge that back into master.

--- FormulaEngine/Evaluate.hs
+++ FormulaEngine/Evaluate.hs
@@ -4,7 +4,7 @@
 
 module FormulaEngine.Evaluate
 (
- eval,
+ calcTree,
  calcCell
 )
 
@@ -15,20 +15,33 @@ import qualified Tree.FormulaTree as T
 import qualified Data.Sheet as S
 import qualified Data.SheetLayout as L
 
--- FIXME: Eval needs to be enhanced for sheet evaluation with references
+
+-- | Type to encapsulate evaluates with and without sheet references.
+data EvalRef = NoRef -- ^ Eval formula tree without sheet references.
+             | SheetRef S.RawSheet -- ^ Eval with sheet
+             | FullRef S.RawSheet [L.Address] -- ^ Full eval with sheet reference and list of used cells.
+                                               -- Params are sheet reference and a list of
+                                               -- previously evaluated cells to check for circular references.
 
 -- | Evaluate FormulaTree.
 eval :: T.FormulaTree -- ^ Compiled tree to evaluate
+     -> EvalRef -- ^ The sheet, if other cells are referenced
      -> P.Plain -- ^ Plain result value
-eval (T.Raw v) = v
-eval (T.TreeError e) = P.PlError $ show e
-eval (T.Funcall f l) = let argv = map eval l
-                           res = f argv
-                       in
-                         case res of
-                           P.PlError _ -> findError res argv
-                           otherwise -> res
-eval (T.Reference addr) = P.PlError $ "Cannot evaluate address without a sheet - referenced:" ++ show addr ++ "."
+eval (T.Raw v) _ = v
+eval (T.TreeError e) _ = P.PlError $ show e
+eval (T.Funcall f l) r = let argv = map ((flip eval) r) l
+                             res = f argv
+                         in
+                           case res of
+                             P.PlError _ -> findError res argv
+                             otherwise -> res
+eval (T.Reference a) (FullRef s xs) = if a `elem` xs then
+                                          P.PlError $ "Circular reference. Twice referenced:" ++ show a
+                                      else
+                                          eval (S.getCell s a) (FullRef s (a:xs))
+eval r@(T.Reference a) (SheetRef s) = eval r (FullRef s [])
+eval (T.Reference a) (NoRef) = P.PlError $ "Cannot evaluate address without a sheet - referenced:" ++ show a ++ "."
+
 
 -- | Check if error already in input or in result of function
 findError :: P.Plain -- ^ Result error value
@@ -41,18 +54,14 @@ findError r argv = let errors = filter P.checkError argv
                      else
                          r
 
--- FIXME: implement an eval' that gets a tree and a sheet and informations catching circular references
--- Possibly use a typeclass and a type constructor to make the current eval a special instance of the
--- new eval (with something throwing an error on no reference).
 
 -- | Calculate one cell in a sheet and return Plain value.
 calcCell :: S.RawSheet -- ^ The sheet to calculate a cell
          -> L.Address -- ^ The cell address we are interested in
          -> P.Plain -- ^ Plain value suitable for show
-calcCell sheet addr = let c = S.getCell sheet addr
-                      in
-                        case c of
-                          T.Raw v -> v
-                          T.Reference a -> calcCell sheet a -- FIXME: Crashes on circular references
-                          T.TreeError e -> P.PlError $ show e
-                          otherwise -> P.PlError "Combinations not yet implemented"
+calcCell  sheet addr = eval (T.Reference addr) (SheetRef sheet)
+
+-- | Calculate a tree - without a sheet reference. Useful for tests only.
+calcTree :: T.FormulaTree -- ^ Tree for evaluation.
+         -> P.Plain -- ^ Return value
+calcTree t = eval t NoRef
--- demo/demo_calc_trees.hs
+++ demo/demo_calc_trees.hs
@@ -16,23 +16,23 @@ main :: IO ()
 main = do
   print "Evaluating raw formula tree"
   let rawtree = T.Funcall NumFuncs.add [(T.Raw (P.PlInt 1)), (T.Raw (P.PlInt 2))]
-  print (Eval.eval rawtree)
+  print (Eval.calcTree rawtree)
   print "Done!"
 
   print "Compiling and evaluating add tree with int - should give 6"
   let stringtree1 = Parse.compileTree "(add 1 (add 2 3))"
-  print (Eval.eval stringtree1)
+  print (Eval.calcTree stringtree1)
 
   print "Compiling and evaluating add tree with float - should give 6.3"
   let stringtree2 = Parse.compileTree "(add 1.0 (add 2.1 3.2))"
-  print (Eval.eval stringtree2)
+  print (Eval.calcTree stringtree2)
 
   print "Compiling and evaluating string function containing escaped quotes"
   let stringtree3 = Parse.compileTree "(conc \"A\"\"\" \"BC\")"
-  print (Eval.eval stringtree3)
+  print (Eval.calcTree stringtree3)
 
   print "Compiling tree with references - will fail in eval"
   let reftree1 = Parse.compileTree "'1:1"
-  print (Eval.eval reftree1)
+  print (Eval.calcTree reftree1)
 
   print "All done successfully"
