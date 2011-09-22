821f2130af6ea293a5a751a79e7ca93c31b948b8
tnrangwi <trtoaster@googlemail.com>
2011-09-22 22:12:20 +0200

    Reorganise Sheet for maintanance.

--- Data/Sheet.hs
+++ Data/Sheet.hs
@@ -1,19 +1,23 @@
--- | This file defines data structure in a workheet.
---
+-- | This file defines data structure in a workheet, build and access functions.
+-- 
 -- Author: Thorsten Rangwich. See file <../LICENSE> for details.
 
---FIXME: Define a sheet, additional to RawSheet and use that one externally
+--FIXME: Define a sheet, additional to RawSheet and use that one externally.
+--FIXME: Interate sheet header data, maxrow and maxcol better in sheet.
 
 module Data.Sheet
     (
+     -- * Data types
      RawSheet,
+     RawHeader,
+     -- * Sheet functions
      emptyRawSheet,
-     addCell,
      buildSheet,
+     addCell,
+     getCell,
      maxRow,
      maxCol,
-     getCell,
-     RawHeader,
+     -- * Header functions
      emptyRawHeader,
      addHeaderProperties
     )
@@ -32,9 +36,27 @@ newtype RawSheet = RSheet { rSheet :: (Map.Map L.Address T.FormulaTree) }
 -- | Type for sheet header. Just a properties map. May cange in the future.
 newtype RawHeader = RHeader { rHeader :: (Map.Map String P.Plain) }
 
+------------------
+-- Sheet functions
+------------------
+
 -- | Create new empty sheet - use that for parsing a new sheet
 emptyRawSheet :: RawSheet -- ^ Empty sheet created with defaults
 emptyRawSheet = RSheet Map.empty
+
+
+-- | Create a sheet from (usually parsed) contents
+buildSheet :: RawHeader -- ^ Sheet header
+           -> [[T.FormulaTree]] -- ^ List of rows, rows itself lists of trees.
+           -> Either String RawSheet -- ^ Error message or sheet structure.
+buildSheet header rows =
+    Right . RSheet . Map.fromList $
+              (L.makeAddr (-1) (-1), len rows) : concatMap buildRow (zip [0..] rows)
+        where
+          len = T.Raw . P.PlInt . flip (-) 1 . length
+          buildRow (r, cs) = (L.makeAddr r (-1), len cs) : map (buildCell r) (zip [0..] cs)
+          buildCell r (c, t) = (L.makeAddr r c, t)
+
 
 -- | Add a single cell to a raw sheet. May change and require certain conditions in the future.
 addCell :: L.Address -- ^ Cell address
@@ -43,28 +65,13 @@ addCell :: L.Address -- ^ Cell address
         -> RawSheet -- ^ Updated sheet
 addCell a t = RSheet . Map.insert a t . rSheet
 
--- | Create a sheet from (usually parsed) contents
-buildSheet :: RawHeader -- ^ Sheet header
-           -> [[T.FormulaTree]]
-           -> Either String RawSheet
-buildSheet header rows = -- FIXME: The limited data type currently does not put the header into the sheet datatype
-                         -- FIXME: Put maximum row and maximum cell by row somewhere else!
-    Right . RSheet . Map.fromList $
-              (L.makeAddr (-1) (-1), len rows) : concatMap buildRow (zip [0..] rows)
-        where
-          len = T.Raw . P.PlInt . flip (-) 1 . length
-          buildRow (r, cs) = (L.makeAddr r (-1), len cs) : map (buildCell r) (zip [0..] cs)
-          buildCell r (c, t) = (L.makeAddr r c, t)
 
--- | Return empty header to add properties later.
-emptyRawHeader :: RawHeader
-emptyRawHeader = RHeader Map.empty
+-- | Get content of single cell
+getCell :: RawSheet -- ^ The sheet.
+        -> L.Address -- ^ Cell address.
+        -> T.FormulaTree
+getCell s a = Map.findWithDefault (T.Raw P.PlEmpty) a $ rSheet s
 
--- | Add properties to header.
-addHeaderProperties :: [(String, P.Plain)] -- ^ Property name / value
-                    -> RawHeader -- ^ Old header
-                    -> RawHeader -- ^ updated header
-addHeaderProperties p = RHeader . Map.union (Map.fromList p) . rHeader
 
 -- | Retrieve number of rows in sheet
 maxRow :: RawSheet -- ^ The sheet.
@@ -81,8 +88,18 @@ maxCol s r = case Map.lookup (L.makeAddr r (-1)) $ rSheet s of
                Just (T.Raw (P.PlInt i)) -> i
                otherwise -> -1
 
--- | Get content of single cell
-getCell :: RawSheet -- ^ The sheet.
-        -> L.Address -- ^ Cell address.
-        -> T.FormulaTree
-getCell s a = Map.findWithDefault (T.Raw P.PlEmpty) a $ rSheet s
+
+--------------------
+-- Header functions.
+--------------------
+
+-- | Return empty header to add properties later.
+emptyRawHeader :: RawHeader
+emptyRawHeader = RHeader Map.empty
+
+-- | Add properties to header.
+addHeaderProperties :: [(String, P.Plain)] -- ^ Property name / value
+                    -> RawHeader -- ^ Old header
+                    -> RawHeader -- ^ updated header
+addHeaderProperties p = RHeader . Map.union (Map.fromList p) . rHeader
+
