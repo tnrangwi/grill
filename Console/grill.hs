4846768d85184de26f8a9b4f94c3d2ed99e92e71
tnrangwi <trtoaster@googlemail.com>
2011-10-20 20:48:49 +0200

    Main menu works in Console - no functionality yet.

--- Console/grill.hs
+++ Console/grill.hs
@@ -5,14 +5,27 @@
 -- | Author: Thorsten Rangwich. See file <../LICENSE> for details.
 
 import qualified System
+import qualified System.Exit as Exit
 import qualified System.Console.GetOpt as GetOpt
 import qualified Data.Map as Map
 import qualified Data.Maybe as Maybe
 
+import qualified System.IO as FileIO
+
+import qualified FormulaEngine.Parse as Parse
+
+import qualified Data.Sheet as Sheet
+
+import Data.String.Utils as StringUtils
+
+-- | String used to mark an argument within the properties.
+argument :: String
+argument = ""
+
 newtype Properties = Prop { prop :: [(String, String)] }
 instance Show Properties where
     show props = let raw = prop props
-                     fa (x,_) = if x == "." then True else False
+                     fa (x,_) = if x == argument then True else False
                      fp = not . fa
                      as = map snd (filter fa raw)
                      ps = filter fp raw
@@ -20,23 +33,29 @@ instance Show Properties where
                    "Properties:\n" ++ (concatMap (\(k,v) -> ("   " ++ k ++ ":" ++ v ++ "\n")) ps) ++
                          "Files:\n" ++ (concatMap (\v -> "   " ++ v ++ "\n") as)
 
-
+-- | Helper function to retrieve argument or property from the properties list.
 getProp' :: String
         -> Properties
         -> [String]
 getProp' n = map snd . filter (\(k,_) -> if k == n then True else False) . prop
 
-getProp n p = if n == "." then [] else getProp' n p
-           
+-- | Get a property (list, if given more than once). Does not return special key used as
+-- marker for arguments instead of properties.
+getProp :: String
+        -> Properties
+        -> [String]
+getProp n p = if n == argument then [] else getProp' n p
+
+-- | Retrieve a boolean flag from within the properties.
 getFlag :: String
         -> Properties
         -> Bool
-getFlag f = elem (f,"") . prop 
+getFlag f = elem (f,"") . prop
 
-getArguments :: String
-             -> Properties
+-- | Retrieve the argument list from within the properties. These are marked with a special property key.
+getArguments :: Properties
              -> [String]
-getArguments p = getProp' "."
+getArguments = getProp' argument
 
 
 -- | Put property name / value into tuple
@@ -86,10 +105,43 @@ parseOptions argv = do
     (_, _, errs) -> ioError (userError (concat errs ++ GetOpt.usageInfo header options))
         where header = "Usage: " ++ prog ++ " [options]" 
 
-consoleLoop :: IO ()
-consoleLoop = do
+-- | Print message, wait for return to display it
+showMessage :: String -> IO ()
+showMessage m = do
+  putStr $ m ++ "\nPress <Enter> to continue\n"
+  FileIO.getLine
+  return ()
+
+-- | Console main loop: Print command line keys, read command and execute it
+consoleLoop :: Properties -> Sheet.RawSheet -> IO ()
+consoleLoop props sheet = do
   putStr ['\n' | _ <- [0..25]]
-  putStr "[D]ump  [L]oad  [S]ave [E]dit cell"  
+  putStr "[D]ump  [L]oad  [S]ave [E]dit cell [Q]uit\n"
+  command <- FileIO.getLine >>= return . StringUtils.strip
+  case if length command > 0 then command!!0 else ' ' of
+    'q' -> return ()
+    'l' -> showMessage "Load not yet implemented" >> consoleLoop props sheet
+    'd' -> showMessage "Dump not yet implemented" >> consoleLoop props sheet
+    's' -> showMessage "Save not yet implemented" >> consoleLoop props sheet
+    'e' -> showMessage "Edit not yet implemented" >> consoleLoop props sheet
+    ' ' -> consoleLoop props sheet
+    otherwise -> showMessage ("Unrecognised command line:" ++ command) >> consoleLoop props sheet
+
+loadSheet :: [String]
+          -> IO Sheet.RawSheet
+loadSheet names = case length names of
+                    0 -> showMessage "No sheet preloaded" >> return Sheet.emptyRawSheet
+                    1 -> do
+                      let sheetName = names!!0
+                      stream <- FileIO.readFile (names!!0)
+                      let sheet = Parse.compileSheet stream
+                      case sheet of
+                        Left msg -> showMessage ("Parse error in sheet:" ++ msg) >> return Sheet.emptyRawSheet
+                        Right parsedSheet -> showMessage ("Sheet loaded:" ++ sheetName) >> return parsedSheet
+                    otherwise -> do
+                      putStr "Only one sheet supported"
+                      return Sheet.emptyRawSheet
+
 
 -- | Startup ugly command interface
 main :: IO ()
@@ -98,4 +150,6 @@ main = do
   argv <- System.getArgs
   props <- parseOptions argv
   putStr $ "Options:\n" ++ show props
-  putStr "Exiting grill..."
+  sheet <- loadSheet (getArguments props)
+  consoleLoop props sheet
+  putStr "Exiting grill...\n"
