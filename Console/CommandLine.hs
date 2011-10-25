0c0f44f27c45f4f488b1b00922c062ffd78d68dc
tnrangwi <trtoaster@googlemail.com>
2011-10-25 22:49:15 +0200

    Separate command line option parser from main loop code

---
+++ Console/CommandLine.hs
@@ -0,0 +1,139 @@
+-- | This file implements command line tools for startup of grill.
+-- In fact it currently is a general command line option parser.
+-- 
+-- | Author: Thorsten Rangwich. See file <../LICENSE> for details.
+
+module Console.CommandLine
+(
+ -- * Accessing command line options.
+ Properties,
+ getFlag,
+ getProp,
+ getArguments,
+ -- * Parsing command line
+ PropertyType(..),
+ Argument(..),
+ parseOptions,
+ parseCmdLine
+)
+where
+
+import qualified System
+import qualified System.Exit as Exit
+import qualified System.Console.GetOpt as GetOpt
+
+import qualified Data.Map as Map
+import qualified Data.Maybe as Maybe
+
+
+-- Properties implementation
+
+-- | Type storing command line properties. Properties are named by string
+-- and may occur more than once. The empty string marks command line
+-- arguments.
+newtype Properties = Prop { prop :: [(String, String)] }
+instance Show Properties where
+    show props = let raw = prop props
+                     fa (x,_) = x == argument
+                     fp = not . fa
+                     as = map snd (filter fa raw)
+                     ps = filter fp raw
+                 in
+                   "Properties:\n" ++ concatMap (\(k,v) -> ("   " ++ k ++ ":" ++ v ++ "\n")) ps ++
+                         "Files:\n" ++ concatMap (\v -> "   " ++ v ++ "\n") as
+
+-- | Empty string used to mark an argument instead of a property within the properties.
+argument :: String
+argument = ""
+
+-- | Helper function to retrieve argument or property from the properties list.
+-- This is implementation and is not exported.
+getKey :: String
+       -> Properties
+       -> [String]
+getKey n = map snd . filter (\(k,_) -> k == n) . prop
+
+-- | Get a property (list, if given more than once). Use getArguments to retrieve
+-- command line arguments instead of command line options.
+getProp :: String
+        -> Properties
+        -> [String]
+getProp n p = if n == argument then [] else getKey n p
+
+-- | Retrieve a command line option without an argument.
+getFlag :: String
+        -> Properties
+        -> Bool
+getFlag f = elem (f,"") . prop
+
+-- | Retrieve the command line arguments from the properties.
+getArguments :: Properties
+             -> [String]
+getArguments = getKey argument
+
+-- Command line parsing
+
+-- | Helper function to put property name / value into tuple
+option :: String -- ^ Property name
+          -> String -- ^ Property value
+          -> (String, String)
+option = (,)
+
+-- | Put flag with default into tuple
+flag :: String -- ^ Flag name
+        -> (String, String)
+flag = (`option` [])
+
+
+-- specifying command line
+
+-- | Data constructor to specify type of command line option.
+data PropertyType = Flag -- ^ Command line option is a flag without parameter.
+                  | OptOpt String String -- ^ Command line option is a flag with optional parameter description
+                                         -- and default value.
+                  | ReqOpt String -- ^ Command line option is a flag with a required parameter description.
+
+-- | Data constructor to specify one command line argument.
+data Argument = Argument {
+      shortNames :: String -- ^ String with all short option characters matching this option / flag.
+      ,longNames :: [String] -- ^ List of long option strings matching this option / flag.
+      ,description :: String -- ^ Description of this command line option.
+      ,propertyName :: String -- ^ Name of the property to identify this option within properties.
+      ,propertyType :: PropertyType -- ^ Type of this command line option.
+    }
+
+
+-- | Convert option specification given as list to option specification useable by GetOpt.
+optionSpec2GetOpt :: [Argument]
+                  -> [GetOpt.OptDescr (String, String)]
+optionSpec2GetOpt [] = []
+optionSpec2GetOpt (x:xs) = (convert x):(optionSpec2GetOpt xs)
+    where convert x = GetOpt.Option
+                      (shortNames x)
+                      (longNames x)
+                      (convertType (propertyName x) (propertyType x))
+                      (description x)
+          convertType n t = case t of
+                              Flag -> GetOpt.NoArg (flag n)
+                              OptOpt desc dflt -> GetOpt.OptArg (option n . Maybe.fromMaybe dflt) desc
+                              ReqOpt desc -> GetOpt.ReqArg (option n) desc
+
+
+-- | Parse options and arguments into properties.
+parseOptions :: [Argument] -- ^ Options specification
+             -> [String] -- ^ Command line arguments
+             -> IO Properties -- ^ Parsed properties
+parseOptions optionSpec argv = do
+  let options = optionSpec2GetOpt optionSpec
+  prog <- System.getProgName
+  case GetOpt.getOpt GetOpt.Permute options argv of
+    (opts, args, [])   -> return $ Prop $ opts ++ map (\x -> (".", x)) args
+    (_, _, errs) -> ioError (userError (concat errs ++ GetOpt.usageInfo header options))
+        where header = "Usage: " ++ prog ++ " [options]" 
+
+-- | Parse command line and return properties for further processing.
+-- further options for debugging are processed.
+parseCmdLine :: [Argument] -- ^ Options speficication
+             -> IO Properties -- ^ Parsed object to be used for queries
+parseCmdLine options = System.getArgs >>= (parseOptions options)
+
--- Console/grill.hs
+++ Console/grill.hs
@@ -4,106 +4,32 @@
 -- 
 -- | Author: Thorsten Rangwich. See file <../LICENSE> for details.
 
-import qualified System
-import qualified System.Exit as Exit
-import qualified System.Console.GetOpt as GetOpt
-import qualified Data.Map as Map
 import qualified Data.Maybe as Maybe
-
+import Data.String.Utils as StringUtils
 import qualified System.IO as FileIO
 
+import qualified Console.CommandLine as Cmd
 import qualified FormulaEngine.Parse as Parse
-
 import qualified Data.Sheet as Sheet
 
-import Data.String.Utils as StringUtils
+-- | Command line option description for grill.
+-- List of Option constructor from System.GetOpt taking the following arguments:
+-- * String with all characters mapped to that short option
+-- * List of strings with all long option words mapped to that option
+-- * Argument specification with NoArg / OptArg (optional argument) or ReqArg and name in properties.
+-- Use flag for NoArg and option for ReqArg or OptArg with a default value.
+-- * Description if user enters invalid command line parameters.
+-- FIXME: Using GetOpt here is ugly, a list should be enough.
 
--- | String used to mark an argument within the properties.
-argument :: String
-argument = ""
-
-newtype Properties = Prop { prop :: [(String, String)] }
-instance Show Properties where
-    show props = let raw = prop props
-                     fa (x,_) = x == argument
-                     fp = not . fa
-                     as = map snd (filter fa raw)
-                     ps = filter fp raw
-                 in
-                   "Properties:\n" ++ concatMap (\(k,v) -> ("   " ++ k ++ ":" ++ v ++ "\n")) ps ++
-                         "Files:\n" ++ concatMap (\v -> "   " ++ v ++ "\n") as
-
--- | Helper function to retrieve argument or property from the properties list.
-getProp' :: String
-        -> Properties
-        -> [String]
-getProp' n = map snd . filter (\(k,_) -> k == n) . prop
-
--- | Get a property (list, if given more than once). Does not return special key used as
--- marker for arguments instead of properties.
-getProp :: String
-        -> Properties
-        -> [String]
-getProp n p = if n == argument then [] else getProp' n p
-
--- | Retrieve a boolean flag from within the properties.
-getFlag :: String
-        -> Properties
-        -> Bool
-getFlag f = elem (f,"") . prop
-
--- | Retrieve the argument list from within the properties. These are marked with a special property key.
-getArguments :: Properties
-             -> [String]
-getArguments = getProp' argument
-
-
--- | Put property name / value into tuple
-mapOption :: String -- ^ Property name
-          -> String -- ^ Property value
-          -> (String, String)
-mapOption = (,)
-
--- | Put flag with default into tuple
-mapFlag :: String -- ^ Flag name
-        -> (String, String)
-mapFlag = (`mapOption` [])
-
--- | Command line option description
-options :: [GetOpt.OptDescr (String, String)]
+options :: [Cmd.Argument]
 options =
     [
-     GetOpt.Option "v?" ["version"]
-               (GetOpt.NoArg (mapFlag "ShowVersion"))
-               "show version",
-
-     GetOpt.Option "c" ["console"]
-               (GetOpt.NoArg (mapFlag "Console"))
-               "no GUI - use console",
-
-     GetOpt.Option "d" ["debug"]
-               (GetOpt.OptArg (mapOption "DebugLevel" . Maybe.fromMaybe "0") "0|1|2")
-               "debug on - pass 1 or 2 to increase level",
-
-     GetOpt.Option "f" ["output-filter"]
-               (GetOpt.ReqArg (mapOption "OutputFilter") "TSV|CSV|PP")
-               "Output filter - TSV, CSV, PP",
-
-     GetOpt.Option "o" ["dump-file"]
-               (GetOpt.NoArg (mapFlag "Dump"))
-               "output contents and exit"
+     Cmd.Argument "v?" ["version"] "show version" "ShowVersion" Cmd.Flag
+    ,Cmd.Argument "c" ["console"] "no GUI - use console" "Console" Cmd.Flag
+    ,Cmd.Argument "d" ["debug"] "debug on - pass number to increase level" "DebugLevel" (Cmd.OptOpt "0|1|2" "0")
+    ,Cmd.Argument "f" ["output-filter"] "Output filter - TSV, CSV, PP" "OutputFilter" (Cmd.ReqOpt "TSV|CSV|PP")
+    ,Cmd.Argument "o" ["dump-file"] "output contents and exit" "Dump" Cmd.Flag
     ]
-
-
--- | Parse options and arguments into properties.
-parseOptions :: [String] -- ^ Command line arguments
-             -> IO Properties -- ^ Parsed properties
-parseOptions argv = do
-  prog <- System.getProgName
-  case GetOpt.getOpt GetOpt.Permute options argv of
-    (opts, args, [])   -> return $ Prop $ opts ++ map (\x -> (".", x)) args
-    (_, _, errs) -> ioError (userError (concat errs ++ GetOpt.usageInfo header options))
-        where header = "Usage: " ++ prog ++ " [options]" 
 
 -- | Print message, wait for return to display it
 showMessage :: String -> IO ()
@@ -113,7 +39,8 @@ showMessage m = do
   return ()
 
 -- | Console main loop: Print command line keys, read command and execute it
-consoleLoop :: Properties -> Sheet.RawSheet -> IO ()
+-- FIXME: Do not use command line properties. Use something else!
+consoleLoop :: Cmd.Properties -> Sheet.RawSheet -> IO ()
 consoleLoop props sheet = do
   putStr ['\n' | _ <- [0..25]]
   putStr "[D]ump  [L]oad  [S]ave [E]dit cell [Q]uit\n"
@@ -148,9 +75,8 @@ loadSheet names = case length names of
 main :: IO ()
 
 main = do
-  argv <- System.getArgs
-  props <- parseOptions argv
+  props <- Cmd.parseCmdLine options
   putStr $ "Options:\n" ++ show props
-  sheet <- loadSheet (getArguments props)
+  sheet <- loadSheet (Cmd.getArguments props)
   consoleLoop props sheet
   putStr "Exiting grill...\n"
