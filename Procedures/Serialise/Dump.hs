639debe29f21ffc0c2365df51d76e7cf319158d1
tnrangwi <trtoaster@googlemail.com>
2011-12-26 16:24:19 +0100

    Removed duplicates in code for dump. Christian: Not a single line of duplicate code left...

--- Procedures/Serialise/Dump.hs
+++ Procedures/Serialise/Dump.hs
@@ -21,37 +21,53 @@ import qualified Data.Sheet as S
 import qualified Data.SheetLayout as L
 import qualified FormulaEngine.Evaluate as E
 
--- | Help stub to build lists
-buildListX :: (Num a, Enum a, Ord a) =>
-             a -- ^ Maximum in list
-          -> [a] -- ^ The list from 0..max-1 or [] if max is 0
-buildListX n = if n > 0 then [0..n - 1] else []
 
--- | Dump objects in a more or less suitable way
+-- | Dump objects in a more or less suitable way. Either evaluated or raw.
 class Dump a b where
-    dump :: a -> b
-    eval :: a -> b
+
+    -- | Dump unevaluated.
+    dump :: a -- ^ Arbitrary input type to dump.
+         -> b -- ^ Arbitrary output type.
+
+    -- | Dump evaluated.
+    eval :: a -- ^ Arbitrary input type to calculate.
+         -> b -- ^ Arbitrary outout type.
+
+    -- | Pretty print evaluated dump.
+    pprint :: a -- ^ Arbitrary input type for nice evaluated print.
+           -> b -- ^ Arbitrary output type.
+    pprint a = error "Pretty print not yet implemented"
+
 
 instance Dump S.Sheet (IO ()) where
 
     dump sheet = putStr (dump sheet :: String)
 
-    eval sheet = let line = replicate 79 '=' ++ "\n"
-                 in
-                   putStr $ line ++ (eval sheet :: String) ++ line
+    eval sheet = putStr (eval sheet :: String)
 
--- FIXME: Replace by Data.Text
+
+-- | Help type to do all dumps with one function. Put everything in one function to reduce duplicate code.
+data DumpType = DumpRaw
+              | DumpEvaluated
+              | DumpPretty
+
+-- | Only buildrow is different for every dump type.
+dump' :: DumpType
+      -> S.Sheet
+      -> String
+dump' t sheet = concat [buildRow r | r <- buildList (S.numRows sheet) ]
+        where
+          buildList n = if n > 0 then [0..n - 1] else []
+          nCols = flip S.numCols sheet
+          buildRow r = case t of
+                         DumpRaw -> List.intercalate "\t" 
+                                     [E.showTree (S.getCell sheet (L.makeAddr r c)) | c <- buildList (nCols r)]
+                                    ++ "\n"
+                         DumpEvaluated -> show [show (E.calcCell sheet (L.makeAddr r c)) | c <- buildList (nCols r)] ++ "\n"
+                         _ -> error "Not yet implemented for this dump type"
+
+
 instance Dump S.Sheet String where
-    dump sheet = concat [buildRow r | r <- buildList (S.numRows sheet) ]
-        where
-          buildList n = if n > 0 then [0..n - 1] else []
-          nCols = flip S.numCols sheet
-          buildRow r = List.intercalate "\t" 
-                                       [E.showTree (S.getCell sheet (L.makeAddr r c)) | c <- buildList (nCols r )]
-                       ++ "\n"
+    dump = dump' DumpRaw
 
-    eval sheet = concat [buildRow r | r <- buildList (S.numRows sheet) ]
-        where
-          buildList n = if n > 0 then [0..n - 1] else []
-          nCols = flip S.numCols sheet
-          buildRow r = show [show (E.calcCell sheet (L.makeAddr r c)) | c <- buildList (nCols r) ] ++ "\n"
+    eval = dump' DumpEvaluated
