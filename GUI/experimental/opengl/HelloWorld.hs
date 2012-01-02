359b13d1f6e3b88ab22f171414fff7018edc7d5e
tnrangwi <trtoaster@googlemail.com>
2012-01-02 23:15:56 +0100

    Add reshape callback - as in tutorial

--- GUI/experimental/opengl/HelloWorld.hs
+++ GUI/experimental/opengl/HelloWorld.hs
@@ -3,11 +3,22 @@
 
 import Graphics.Rendering.OpenGL
 import Graphics.UI.GLUT
+
+myPoints :: [(GLfloat, GLfloat, GLfloat)]
+myPoints = map (\k -> (sin(2*pi*k/12), cos(2*pi**k/12), 0.0))[1..12]
+
 main = do
   (progname, _) <- getArgsAndInitialize
   createWindow "Hello, world!"
   displayCallback $= display
+  reshapeCallback $= Just reshape
   mainLoop
 
 display = do
-  clear [ ColorBuffer ] ; flush
+  clear [ ColorBuffer ]
+  renderPrimitive Points $ mapM_(\(x, y, z) -> vertex$Vertex3 x y z) myPoints
+  flush
+
+reshape s@(Size w h) = do
+  viewport $= (Position 0 0, s)
+  postRedisplay Nothing
